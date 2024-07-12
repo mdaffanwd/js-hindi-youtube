480432944fb0bbffabac807d58cfe216fde042fc
mdaffanwd <mohammedaffan2305@gmail.com>
2024-07-12 10:40:30 +0530

    explantion in call.js

--- 10_classes_and_oop/call.js
+++ 10_classes_and_oop/call.js
@@ -6,7 +6,14 @@ function SetUsername(username){
 
 function createUser(username, email, password){
     SetUsername.call(this, username)
-   
+     /*
+  ****** From ChatGPT *
+SetUsername.call(this, username);:
+call method ke pehle argument (this) se, aap specify karte hain ki SetUsername function ke andar this keyword ka value kya hona chahiye.
+Is case mein, this naye object ko refer kar raha hai jo createUser function ke execution ke dauran banaya gaya tha.
+SetUsername function ke andar, this.username = username; statement naye object ke username property ko set karta hai.
+console.log("called"); statement execute hota hai, aur console pe "called" print hota hai.
+  */
     this.email = email
     this.password = password
 }
